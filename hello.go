d0e3b09054f45e4c84db21faf5259dfc21838399
Gustavo Gama <gustavogama494@gmail.com>
2023-12-22 16:08:56 -0300

    modulo 2

--- hello.go
+++ hello.go
@@ -1,8 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
+	nome := "Douglas"
+	idade := 24
+	versao := 1.1
 
-	fmt.Println("Olá mundo")
+	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("o tipo da variavel é", reflect.TypeOf(nome))
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do programa")
+
+	var comando int
+	fmt.Scan(comando)
+
+	fmt.Println("O comando escolhido foi", comando)
+
 }
