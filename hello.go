b50b14ff13b29bc42b06458d9700c46ce96ccee2
Gustavo Gama <gustavogama494@gmail.com>
2023-12-22 17:02:28 -0300

    modulo 3

--- hello.go
+++ hello.go
@@ -2,26 +2,46 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"os"
 )
 
 func main() {
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+}
+
+func exibeIntroducao() {
 	nome := "Douglas"
 	idade := 24
 	versao := 1.1
 
 	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
+}
 
-	fmt.Println("o tipo da variavel é", reflect.TypeOf(nome))
+func leComando() (comando int) {
+	fmt.Scan(&comando)
+	fmt.Println("O comando escolhido foi", comando)
+	return comando
+}
 
+func exibeMenu() {
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do programa")
-
-	var comando int
-	fmt.Scan(comando)
-
-	fmt.Println("O comando escolhido foi", comando)
 
 }
